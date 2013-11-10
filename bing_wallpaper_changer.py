7c3d222e4a7d5126b75400f39bc043a540870b98
Gary Hughes <gary@overthere.co.uk>
2013-11-10 21:27:40 +0000

    Added command line switch to close an already running copy of the program. To be used in the uninstaller.

--- bing_wallpaper_changer.py
+++ bing_wallpaper_changer.py
@@ -4,11 +4,15 @@ Copyright (c) 2013 Gary Hughes
 """
 import StringIO
 import argparse
+import os
+import subprocess
 
 import sys
+import platform
 
 from PyQt4.QtCore import QLocale, QSharedMemory
 from PyQt4.QtGui import QApplication, QStyleFactory, QIcon, QPixmap
+import signal
 
 from ui.mainwindow import MainWindow
 from ui.message_boxes import message_box_ok, message_box_error
@@ -21,6 +25,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Bing Wallpaper Changer', add_help=False)
 
     parser.add_argument('--quit', dest='quit', action='store_true', default=False, help='update wallpaper and exit')
+    parser.add_argument('--quit-existing', dest='quit_existing', action='store_true', default=False,
+                        help='close already running process')
     parser.add_argument('--help', dest='help', action='store_true', help='show help window')
     parser.add_argument('--version', action='version', version='Bing Wallpaper Changer %s' % VERSION)
 
@@ -53,9 +59,26 @@ def parse_arguments():
 def instance_check(app):
     app.instance_check = QSharedMemory('Bing Wallpaper Changer - Shared Memory Check')
     if not app.instance_check.create(10):
-        # Already running...
-        return False
-    return True
+        # Already running. Read the PID from the shared memory and return it.
+        app.instance_check.attach(QSharedMemory.ReadOnly)
+        app.instance_check.lock()
+        existing_pid = str(buffer(app.instance_check.data()))[:10].strip()
+
+        try:
+            existing_pid = int(existing_pid)
+        except ValueError:
+            existing_pid = 0
+
+        app.instance_check.unlock()
+        return existing_pid
+
+    # Write the current PID into the shared memory object.
+    app.instance_check.lock()
+    view = memoryview(app.instance_check.data())
+    pid = str(app.applicationPid())
+    view[0:10] = '{: <10}'.format(pid)
+    app.instance_check.unlock()
+    return 0
 
 
 def startmain():
@@ -83,11 +106,19 @@ def startmain():
     app.args = parse_arguments()
     print 'Args:', app.args
 
-    if not instance_check(app):
-        message_box_error('Program already running.',
-                          'You can only have one copy of the Bing Wallpaper Changer running at once.')
+    # Check to see if application already running.
+    existing_pid = instance_check(app)
+    if existing_pid:
+        if app.args.quit_existing:
+            # Command line argument passed to close existing program. Do that, then quit.
+            if platform.system() == "Windows":
+                subprocess.Popen("taskkill /F /T /PID %i" % existing_pid, shell=True)
+            else:
+                os.killpg(existing_pid, signal.SIGKILL)
+        else:
+            message_box_error('Program already running.',
+                              'You can only have one copy of the Bing Wallpaper Changer running at once.')
         sys.exit()
-
 
     mainwindow = MainWindow()
     # mainwindow.show()
