55e8ea53f3ea2c1510fc409c77ad22205da56fd5
Gary <Gary@XPS>
2013-11-10 20:28:38 +0000

    Added check to see if the program is already running to prevent multiple instances.

--- ui/message_boxes.py
+++ ui/message_boxes.py
@@ -50,3 +50,21 @@ def message_box_ok(text, informative_text, title=None, icon=QMessageBox.Informat
     layout = msg_box.layout()
     layout.addItem(horizontal_spacer, layout.rowCount(), 0, 1, layout.columnCount())
     return msg_box.exec_()
+
+
+def message_box_error(text, informative_text, title=None, icon=QMessageBox.Critical):
+    msg_box = QMessageBox()
+    msg_box.setText('<b>%s</b>' % text)
+    if informative_text:
+        msg_box.setInformativeText(informative_text)
+    msg_box.setIcon(icon)
+    if title:
+        msg_box.setWindowTitle(title)
+    else:
+        msg_box.setWindowTitle(QApplication.instance().applicationName())
+    msg_box.setStandardButtons(QMessageBox.Ok)
+    msg_box.setDefaultButton(QMessageBox.Ok)
+    horizontal_spacer = QSpacerItem(400, 0, QSizePolicy.Minimum, QSizePolicy.Expanding)
+    layout = msg_box.layout()
+    layout.addItem(horizontal_spacer, layout.rowCount(), 0, 1, layout.columnCount())
+    return msg_box.exec_()
