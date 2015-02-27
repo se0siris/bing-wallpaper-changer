ce3e8629d43a068ed11c3c409902a2ef173d54a0
Gary Hughes <gary.hughes@pearl-scan.co.uk>
2015-02-27 10:28:07 +0000

    Added "feh" support. Small cleanups.

--- ui/wallpaper_changer.py
+++ ui/wallpaper_changer.py
@@ -7,7 +7,6 @@ __author__ = 'gary'
 
 
 class WallpaperChanger(object):
-
     def __init__(self):
         self.settings = Settings()
         self.process = QProcess()
@@ -19,10 +18,12 @@ class WallpaperChanger(object):
         elif system_platform == 'Linux':
             # Read desktop environment from settings.
             env = self.settings.linux_desktop
-            print env
-            if env == 'unity':
+            print 'Setting wallpaper using environment "{0:s}"'.format(env)
+            if env == 'feh':
+                return self._feh(filepath)
+            elif env == 'unity':
                 return self._unity(filepath)
-            if env == 'xfce4':
+            elif env == 'xfce4':
                 return self._xfce4(filepath)
 
     def _windows(self, filepath):
@@ -31,8 +32,12 @@ class WallpaperChanger(object):
         SPI_SETDESKWALLPAPER = 20  # According to http://support.microsoft.com/default.aspx?scid=97142
         ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, filepath, 1)
 
+    def _feh(self, filepath):
+        error = self.process.execute('feh --bg-scale {0:s)'.format(filepath))
+        return not bool(error)
+
     def _unity(self, filepath):
-        error = self.process.execute('gsettings set org.gnome.desktop.background picture-uri ' + filepath)
+        error = self.process.execute('gsettings set org.gnome.desktop.background picture-uri {0:s}'.format(filepath))
         return not bool(error)
 
     def _xfce4(self, filepath):
@@ -44,19 +49,10 @@ class WallpaperChanger(object):
         for item in properties:
             self.process.start('xfconf-query --channel xfce4-desktop --property {0:s} --set {1:s}'.format(item, '/'))
             self.process.waitForFinished()
-            self.process.start('xfconf-query --channel xfce4-desktop --property {0:s} --set {1:s}'.format(item, filepath))
+            self.process.start(
+                'xfconf-query --channel xfce4-desktop --property {0:s} --set {1:s}'.format(item, filepath))
             self.process.waitForFinished()
             if self.process.exitCode():
                 error = True
         return not error
 
-
-def main():
-    wallpaper_changer = WallpaperChanger()
-    print wallpaper_changer._xfce4('/home/gary/Pictures/Bing/20150213.jpg')
-
-
-if __name__ == '__main__':
-    import sys
-    main()
-    sys.exit()
